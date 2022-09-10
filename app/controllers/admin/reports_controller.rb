2576a7738c33b193d3561e2d8b616aee476d94e3
hare-nx <aka.nkmr@gmail.com>
2022-09-10 09:38:51 +0000

    [Fix] ReportsController

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,12 +2,12 @@ class Admin::ReportsController < ApplicationController
   before_action :authenticate_admin!
   
   def reported_posts
-    report_post_id=Report.where.not(post_id: nil).each {|p|  p.post_id}
+    report_post_id=Report.where.not(post_id: nil).pluck(:post_id)
     @posts=Post.where(id: report_post_id)
   end
 
   def reported_comments
-    report_comment_id=Report.where.not(comment_id: nil).each {|c| c.comment_id}
+    report_comment_id=Report.where.not(comment_id: nil).pluck(:comment_id)
     @comments=Comment.where(id: report_comment_id)
   end
 end
